7eaec2ac9fceb2caf74f020caaa01b2f02129dcc
Nickleslicka <101658224+LukeAustin24@users.noreply.github.com>
2022-07-09 19:41:43 -0400

    13256

---
+++ Game Jammin n Slammin/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float speed;
+    public float jumpHeight;
+
+    private float moveInput;
+
+    Rigidbody2D rb;
+
+    private bool facingRight = true;
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    private bool isJumping;
+    private float jumpTimeCounter;
+    public float jumpTime;
+
+   // public Animator anim;
+
+
+
+   // public AudioSource[] playerSounds;
+
+    void Start()
+    {
+       // anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    void Update()
+    {
+        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space))
+        {
+           // playerSounds[0].Play();
+           // anim.SetTrigger("takeOff");
+            isJumping = true;
+            jumpTimeCounter = jumpTime;
+            rb.velocity = Vector2.up * jumpHeight;
+        }
+        if (Input.GetKey(KeyCode.Space) && isJumping == true)
+        {
+            if (jumpTimeCounter > 0)
+            {
+                rb.velocity = Vector2.up * jumpHeight;
+                jumpTimeCounter -= Time.deltaTime;
+            }
+            else
+            {
+                isJumping = false;
+
+            }
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isJumping = false;
+            //playerSounds[1].Play();
+        }
+        if (isGrounded == true)
+        {
+           // anim.SetBool("isJumping", false);
+        }
+        else
+        {
+            //anim.SetBool("isJumping", true);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+        moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+        if (facingRight == false && moveInput > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && moveInput < 0)
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+
+        transform.Rotate(0f, 180, 0f);
+    }
+}
