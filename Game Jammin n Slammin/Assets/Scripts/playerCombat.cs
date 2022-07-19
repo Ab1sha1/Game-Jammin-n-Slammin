b069b52096605c74d2703e3ddb73c07e1ab6d5c8
Nickleslicka <101658224+LukeAustin24@users.noreply.github.com>
2022-07-18 21:45:23 -0400

    Fixed Jittering, and added hit reg

---
+++ Game Jammin n Slammin/Assets/Scripts/playerCombat.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCombart : MonoBehaviour
+{
+
+    public Animator animator;
+
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0)) 
+        {
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        //Play an Attack animation
+
+
+        //Detect enemies in range of attack
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        //Damage them
+        foreach(Collider2D enemy in hitEnemies) 
+        {
+            Debug.Log("We hit" + enemy.name);
+        }
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null) 
+        {
+            return;
+        }
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+}
