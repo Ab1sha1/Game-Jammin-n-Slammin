d09a2272f7b022871c11b1180d352291aefec025
Nickleslicka <101658224+LukeAustin24@users.noreply.github.com>
2022-08-21 13:47:38 -0400

    bit o work

--- Game Jammin n Slammin/Assets/Scripts/playerCombat.cs
+++ Game Jammin n Slammin/Assets/Scripts/playerCombat.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerCombart : MonoBehaviour
+public class playerCombat : MonoBehaviour
 {
 
     public Animator animator;
